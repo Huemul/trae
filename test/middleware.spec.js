fe3a7e5d97a6d5aa5ab3312b7ef476562cb9828f
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-12-01 01:32:40 -0300

    Updating test cases

--- test/middleware.spec.js
+++ test/middleware.spec.js
@@ -173,7 +173,7 @@ describe('Middleware -> middleware', () => {
   });
 
   describe('resolveAfter', () => {
-    it('apply changes to response chaining _after functions', () => {
+    it('apply changes to response chaining _after functions after success', () => {
       const middleware = new Middleware();
       const res   = { test: true };
 
@@ -182,7 +182,25 @@ describe('Middleware -> middleware', () => {
         return res;
       });
 
-      return middleware.resolveAfter(res)
+      return middleware.resolveAfter(null, res)
+      .then((res) => {
+        expect(res).toEqual({
+          foo : 'bar',
+          test: true
+        });
+      });
+    });
+
+    it('apply changes to response chaining _after functions after error', () => {
+      const middleware = new Middleware();
+      const err   = { test: true };
+
+      middleware.after((res) => {
+        res.foo = 'bar';
+        return res;
+      });
+
+      return middleware.resolveAfter(err)
       .then((res) => {
         expect(res).toEqual({
           foo : 'bar',

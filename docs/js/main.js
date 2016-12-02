b619ef096cc881e9f119b68cb6c920275bcdba26
gillchristian <gillchristiang@gmail.com>
2016-12-02 14:55:33 -0300

    Update docs example page.

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,25 +3,30 @@
 ((t) => {
 
   const result = document.getElementById('result');
+  const loading = document.getElementById('loading-text');
   const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+  let i = 0;
+  const interval = setInterval(() => {
+    // eslint-disable-next-line prefer-template
+    loading.innerHTML = '// Loading ' + Array(i).fill('.').join('');
+    i = i++ < 3 ? i : 0;
+  }, 200);
 
   function success(res) {
     res.data = null;
-    res = JSON.stringify(res);
     return Promise.resolve(res);
   }
 
-  function after(err, res) {
-    res = res.split(',');
-    return Promise.resolve(res);
-  }
-
-  trae.use({ success, after });
+  trae.use({ success });
 
   trae.get(url)
   .then((res) => {
-    let html = '\n\t\t\t// request final reponse\n\t\t\t';
-    html += res.join('<br>\t\t\t');
+    clearInterval(interval);
+    result.removeChild(loading);
+
+    let html = '\n// request final reponse\n';
+    html += JSON.stringify(res, null, '\t');
     result.innerHTML += html;
   });
 

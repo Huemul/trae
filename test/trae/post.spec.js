895426ecf0f42a4c3d59ecb77b67fdc22e515a66
Nicolas Del Valle <nicolas.delvalle@gmail.com>
2017-02-25 02:27:44 -0300

    Add method with body params support (#54)

--- test/trae/post.spec.js
+++ test/trae/post.spec.js
@@ -14,8 +14,8 @@ describe('trae -> post', () => {
     const url = `${TEST_URL}/foo`;
 
     fetchMock.mock(url, {
-      status : 200,
-      body   : {
+      status: 200,
+      body  : {
         foo: 'bar'
       },
       headers: {
@@ -39,6 +39,77 @@ describe('trae -> post', () => {
       expect(fetchMock.called(url)).toBeTruthy();
       expect(fetchMock.lastUrl()).toBe(url);
       expect(fetchMock.lastOptions().method).toBe('post');
+    });
+  });
+
+  describe('post -> params', () => {
+
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    it('makes a POST request to baseURL + path using params', () => {
+      const url = `${TEST_URL}/foo`;
+      const qs  = '?foo=bar&key=123&token=12345lkjhpor837';
+
+      fetchMock.mock(url + qs, {
+        status : 200,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          foo: 'bar'
+        }
+      }, {
+        method: 'post'
+      });
+
+      return trae.post(url, {
+        sarasa: 'request body'
+      }, {
+        params: {
+          foo  : 'bar',
+          key  : 123,
+          token: '12345lkjhpor837'
+        }
+      })
+      .then((res) => {
+        expect(res).toMatchSnapshot();
+        expect(fetchMock.called(url + qs)).toBeTruthy();
+        expect(fetchMock.lastUrl()).toBe(url + qs);
+        expect(fetchMock.lastOptions().method).toBe('post');
+      });
+    });
+
+    it('makes a POST request to baseURL + path using a nested object as params', () => {
+      const url = `${TEST_URL}/foo`;
+      const qs  = '?a%5Bb%5D=c';
+
+      fetchMock.mock(url + qs, {
+        status : 200,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          foo: 'bar'
+        }
+      });
+
+      return trae.post(url, {
+        sarasa: 'request body'
+      }, {
+        params: {
+          a: {
+            b: 'c'
+          }
+        }
+      })
+      .then((res) => {
+        expect(res).toMatchSnapshot();
+        expect(fetchMock.called(url + qs)).toBeTruthy();
+        expect(fetchMock.lastUrl()).toBe(url + qs);
+        expect(fetchMock.lastOptions().method).toBe('post');
+      });
     });
   });
 });

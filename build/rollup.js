584781b0ff906f8a839d5b07afc2246ea18755b7
Christian Gill <gillchristiang@gmail.com>
2018-03-25 03:43:01 +0200

    Add prettier & update eslint config (#78)
    * Add prettier & pretty-quick, Also remove unncessary eslint rules
    
    * Format code

--- build/rollup.js
+++ build/rollup.js
@@ -1,87 +1,92 @@
-const rollup             = require('rollup');
-const uglify             = require('rollup-plugin-uglify');
-const babel              = require('rollup-plugin-babel');
-const replace            = require('rollup-plugin-replace');
-const eslint             = require('rollup-plugin-eslint');
-const conditional        = require('rollup-plugin-conditional');
-const filesize           = require('rollup-plugin-filesize');
-const resolve            = require('rollup-plugin-node-resolve');
-const commonjs           = require('rollup-plugin-commonjs');
-const visualizer         = require('rollup-plugin-visualizer');
-const builtins           = require('rollup-plugin-node-builtins');
-const globals            = require('rollup-plugin-node-globals');
-const json               = require('rollup-plugin-json');
-const mkdirp             = require('mkdirp');
+const rollup = require('rollup')
+const uglify = require('rollup-plugin-uglify')
+const babel = require('rollup-plugin-babel')
+const replace = require('rollup-plugin-replace')
+const eslint = require('rollup-plugin-eslint')
+const conditional = require('rollup-plugin-conditional')
+const filesize = require('rollup-plugin-filesize')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const visualizer = require('rollup-plugin-visualizer')
+const builtins = require('rollup-plugin-node-builtins')
+const globals = require('rollup-plugin-node-globals')
+const json = require('rollup-plugin-json')
+const mkdirp = require('mkdirp')
 
-const skipCommentsCustom = require('./uglify-skip-comments');
-const generateBanner     = require('./generate-banner');
-const generateBundleName = require('./generate-bundle-name');
-const pkg                = require('../package.json');
+const skipCommentsCustom = require('./uglify-skip-comments')
+const generateBanner = require('./generate-banner')
+const generateBundleName = require('./generate-bundle-name')
+const pkg = require('../package.json')
 
-const env    = process.env.NODE_ENV || 'development';
-const isProd = env === 'production';
+const env = process.env.NODE_ENV || 'development'
+const isProd = env === 'production'
 
-mkdirp('./dist');
-mkdirp('./stats');
+mkdirp('./dist')
+mkdirp('./stats')
 
-let bundles = Promise.resolve();
+let bundles = Promise.resolve()
 
-const inputOptions = format => ({
+const inputOptions = (format) => ({
   input: 'lib/index.js',
   plugins: [
     json({
-      include    : 'package.json',
-      preferConst: true
+      include: 'package.json',
+      preferConst: true,
     }),
     eslint({
-      exclude: 'package.json'
+      exclude: 'package.json',
     }),
     conditional(format === 'cjs', [globals(), builtins()]),
     resolve({
-      jsnext        : format === 'umd',
-      main          : true,
-      browser       : format === 'umd',
-      preferBuiltins: true
+      jsnext: format === 'umd',
+      main: true,
+      browser: format === 'umd',
+      preferBuiltins: true,
     }),
     commonjs(),
     babel({
       babelrc: false, // jest makes use of .babelrc
       presets: [['@babel/preset-env', { modules: false }]],
-      exclude: ['node_modules/**', 'package.json']
+      exclude: ['node_modules/**', 'package.json'],
     }),
     replace({
       exclude: ['node_modules/**', 'package.json'],
       values: {
         'process.env.NODE_ENV': JSON.stringify(env),
-        NODE_ENV              : JSON.stringify(env)
-      }
+        NODE_ENV: JSON.stringify(env),
+      },
     }),
     visualizer({ filename: `./stats/${format}-bundle-statistics.html` }),
-    conditional(isProd && format === 'umd', [uglify({ output: { comments: skipCommentsCustom } })]),
-    filesize()
-  ]
-});
+    conditional(isProd && format === 'umd', [
+      uglify({ output: { comments: skipCommentsCustom } }),
+    ]),
+    filesize(),
+  ],
+})
 
-const outputOptions = format => ({
+const outputOptions = (format) => ({
   format,
-  dir : 'dist',
+  dir: 'dist',
   file: `dist/${generateBundleName(format, isProd)}.js`,
   // The variable name, representing the umd bundle, by which other scripts on the same
   // page can access it
-  name         : format === 'umd' ? pkg.name : undefined,
+  name: format === 'umd' ? pkg.name : undefined,
   sourcemapFile: 'dist',
-  banner       : generateBanner(pkg.version, pkg.author, pkg.contributors)
-});
+  banner: generateBanner(pkg.version, pkg.author, pkg.contributors),
+})
 
-const build = opts => rollup.rollup(opts.input).then(bundle => bundle.write(opts.output));
+const build = (opts) =>
+  rollup.rollup(opts.input).then((bundle) => bundle.write(opts.output))
 
 // cjs – CommonJS, suitable for Node and Browserify/Webpack
 // umd – Universal Module Definition, works as amd, cjs and iife all in one
-['cjs', 'umd'].forEach((format) => {
-  bundles = bundles.then(() => build({
-    input: inputOptions(format),
-    output: outputOptions(format)
-  }));
-});
+;['cjs', 'umd'].forEach((format) => {
+  bundles = bundles.then(() =>
+    build({
+      input: inputOptions(format),
+      output: outputOptions(format),
+    }),
+  )
+})
 
-bundles.catch(err => console.error(err.stack));
+bundles.catch((err) => console.error(err.stack))
--- test/trae/head.spec.js
+++ test/trae/head.spec.js
@@ -1,38 +1,40 @@
 /* global describe it expect afterEach */
 
-import fetchMock from 'fetch-mock';
-import trae      from '../../lib';
+import fetchMock from 'fetch-mock'
+import trae from '../../lib'
 
 afterEach(() => {
-  fetchMock.restore();
-});
+  fetchMock.restore()
+})
 
-const TEST_URL = 'http://localhost:8080/api';
+const TEST_URL = 'http://localhost:8080/api'
 
 describe('trae -> head', () => {
   it('makes a HEAD request to baseURL + path', () => {
-    const url = `${TEST_URL}/foo`;
+    const url = `${TEST_URL}/foo`
 
-    fetchMock.mock(url, {
-      status: 200
-    }, {
-      method: 'head'
-    });
+    fetchMock.mock(
+      url,
+      {
+        status: 200,
+      },
+      {
+        method: 'head',
+      },
+    )
 
-    const testTrae = trae.create();
+    const testTrae = trae.create()
 
     testTrae.before((c) => {
-      expect(c.headers).toEqual({});
-      return c;
-    });
+      expect(c.headers).toEqual({})
+      return c
+    })
 
-
-    return testTrae.head(url)
-    .then((res) => {
-      expect(res).toMatchSnapshot();
-      expect(fetchMock.called(url)).toBeTruthy();
-      expect(fetchMock.lastUrl()).toBe(url);
-      expect(fetchMock.lastOptions().method).toBe('HEAD');
-    });
-  });
-});
+    return testTrae.head(url).then((res) => {
+      expect(res).toMatchSnapshot()
+      expect(fetchMock.called(url)).toBeTruthy()
+      expect(fetchMock.lastUrl()).toBe(url)
+      expect(fetchMock.lastOptions().method).toBe('HEAD')
+    })
+  })
+})

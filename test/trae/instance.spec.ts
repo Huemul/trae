a1b6d6eac5635e351bee58e428eefa5f7dd61870
ndelvalle <nicolas.delvalle@gmail.com>
2020-03-07 20:38:24 +0100

    Update trae create method and tests

--- test/trae/instance.spec.ts
+++ test/trae/instance.spec.ts
@@ -35,5 +35,59 @@ describe('Trae', function () {
         expect(actual).toStrictEqual(expected);
       });
     });
+
+    describe('Inherit configuration', function () {
+      const instance = trae.create({
+        url: 'http://localhost',
+        headers: {
+          'Authorization': 'Basic YWxhZGRpbjpvcGVuc2VzYW1l',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+
+      it('should have configuration headers', () => {
+        const actual = instance.config.headers
+        const expected = {
+          'Authorization': 'Basic YWxhZGRpbjpvcGVuc2VzYW1l',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        expect(actual).toStrictEqual(expected);
+      });
+
+      it('should have http://localhost base url', () => {
+        const actual = instance.config.url
+        const expected = 'http://localhost'
+
+        expect(actual).toStrictEqual(expected);
+      });
+
+      describe('Nested inherit configuration', function () {
+        const nested = instance.create({
+          url: 'http://api.com',
+          headers: {
+            'X-Current-City': 'Barcelona'
+          }
+        });
+
+        it('should have configuration headers', () => {
+          const actual = nested.config.headers
+          const expected = {
+            'Authorization': 'Basic YWxhZGRpbjpvcGVuc2VzYW1l',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-Current-City': 'Barcelona'
+          }
+
+          expect(actual).toStrictEqual(expected);
+        });
+
+        it('should have http://localhost base url', () => {
+          const actual = nested.config.url
+          const expected = 'http://api.com'
+
+          expect(actual).toStrictEqual(expected);
+        });
+      });
+    });
   });
 });

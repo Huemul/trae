d3118fc94593de824d66b81c4419442266c8a16e
gillchristian <gillchristiang@gmail.com>
2020-03-21 10:11:47 +0100

    build: Remove warnings

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,3 @@
-// https://www.npmjs.com/package/tsdx#customization
-// Not transpiled with TypeScript or Babel, so use plain Es6/Node.js
-
 const filesize = require('rollup-plugin-filesize');
 
 module.exports = {
